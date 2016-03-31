52767b457f2ab0a4aa28e6215cc8a58cd3de215e
parkercatalano <parkercatalano@gmail.com>
2016-03-30 19:44:36 -0700

    create functionality for dieRoll var set to playerPoints prototype/display in console.log

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,25 @@ function Player(){
 }
 var player1 = new Player();
 var player2 = new Player();
+var dieRoll = Math.floor(Math.random() * 6) + 1;
 
-Player.prototype.dieRoll = Math.floor(Math.random() * 6) + 1;
+Player.prototype.playerPoints = function(){
+  return dieRoll
+}
   if (dieRoll === 1) {
     turn = false;
 }
-  else if (dieRoll > 1) {
-    alert("Your Rolled a " + dieRoll);
+  else {
+    console.log(dieRoll);
+    turn = true;
   }
 
   //user interface logic...
   //$("ul#player1Score").append(dieRoll);
+  $(document).ready(function() {
+
+  $("#player-roll").submit(function(event) {
+    event.preventDefault();
+    $("ul#player1Score").append(dieRoll);
+  })
+})
