ebb2535215d8aef37554a003fb97f83c371f6c3a
parkercatalano <parkercatalano@gmail.com>
2016-03-30 19:17:21 -0700

    separate business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 
 //business logic
-var turn = true;
-
-function Player(points,hold){
-  this.points = points;
-  this.hold = hold;
+function Player(){
+  this.points = 0;
+  this.turn = true;
 }
+var player1 = new Player();
+var player2 = new Player();
 
-var dieRoll = Math.floor(Math.random() * 6) + 1;
+Player.prototype.dieRoll = Math.floor(Math.random() * 6) + 1;
   if (dieRoll === 1) {
     turn = false;
 }
   else if (dieRoll > 1) {
     alert("Your Rolled a " + dieRoll);
-    $("ul#player1Score").append(dieRoll);
   }
+
+  //user interface logic...
+  //$("ul#player1Score").append(dieRoll);
