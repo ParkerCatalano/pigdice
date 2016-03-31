18bfd0ef82edcc916219cd4ece555b7b25aad0d3
parkercatalano <parkercatalano@gmail.com>
2016-03-30 20:52:00 -0700

    add dieRoll prototype and change other prototype to updateScore

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,17 @@
 function Player(){
   this.points = 0;
   this.turn = true;
+  this.dieRoll = 0;
 }
 var player1 = new Player();
 var player2 = new Player();
 var dieRoll = Math.floor(Math.random() * 6) + 1;
 
-Player.prototype.playerPoints = function(){
-  return dieRoll
+Player.prototype.dieRoll = function() {
+  this.dieRoll = random;
+}
+Player.prototype.updateScore = function(){
+  return this.points += dieRoll;
 }
   if (dieRoll === 1) {
     turn = false;
@@ -25,6 +29,6 @@ Player.prototype.playerPoints = function(){
   event.preventDefault();
   $("#player-roll").submit(function(event) {
 
-    $("ul#player1Score").append(dieRoll);
+    $("#player1Score.score1").append(playerPoints);
   })
 })
